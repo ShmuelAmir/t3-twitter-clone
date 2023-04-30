8c9e1297ad0a832723afd63a961c80c6b1a31892
ShmuelAmir <shmulik5777@gmail.com>
2023-04-30 12:26:16 +0300

    Profile page with SSG helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import type { AppType } from "next/app";
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from 'react-hot-toast'
@@ -9,6 +10,11 @@ import "~/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps} >
+      <Head>
+        <title>Chirp</title>
+        <meta name="description" content="🤔" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position='bottom-center' />
       <Component {...pageProps} />
     </ClerkProvider>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,3 @@
-import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -10,6 +9,7 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { filterUserForClient } from '~/server/helpers/filterUserForClient';
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
@@ -17,13 +17,7 @@ const ratelimit = new Ratelimit({
   analytics: true,
 });
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
+
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
